8b2187a1bd50873603d367c3c7aa8239862a1bf4
zane <zanen@inbox.lv>
2019-07-09 17:18:42 +0300

    exportExcel

--- src/main/java/com/example/demo/services/OrganizerServiceImpl.java
+++ src/main/java/com/example/demo/services/OrganizerServiceImpl.java
@@ -5,7 +5,9 @@ import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.model.Marathon;
 import com.example.demo.model.Organizer;
+import com.example.demo.repo.MarathonRepo;
 import com.example.demo.repo.OrganizerRepo;
 
 import java.io.FileNotFoundException;
@@ -13,6 +15,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.RichTextString;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -22,6 +25,8 @@ public class OrganizerServiceImpl implements OrganizerService{
 
 	@Autowired
 	OrganizerRepo organizerRepo;
+	@Autowired
+	MarathonRepo marathonRepo;
 	
 	@Override
 	public ArrayList<Organizer> selectAll() {
@@ -95,22 +100,61 @@ public class OrganizerServiceImpl implements OrganizerService{
 
 	        XSSFWorkbook workbook = new XSSFWorkbook();
 	        XSSFSheet sheet = workbook.createSheet("Marathon info");
-	        Object[][] datatypes = {
-	                {"Name", "Place", "Distance"},
-	                {"Big", "Jelgava", 2},
+	        
+	        
+	        ArrayList<Marathon> tempList = new ArrayList<Marathon>();
+			for (Marathon m:marathonRepo.findAll())
+			{
+				tempList.add(m);
+			}
+			/*
+	        ArrayList<Marathon> tempList = new ArrayList<Marathon>();
+			for (Marathon m:marathonRepo.findAll())
+			{
+				tempList.add(m);
+			}
+			int k=(int) marathonRepo.count();
+			Marathon[][] maratoni =new Marathon [k][3];
+			for (int i=0; i<k;i++)
+			{
+				for(int j=0; j<3;j++)
+				{
+					maratoni[i][j];
+				}
+			}
+			*/
+			
+			
+			/*maratoni = {
+	                {maratoni.getName(), maratoni.getPlace(), maratoni.getDistance()},
+	                {"Big", "Jelgava", 7},
 	                {"Tet", "Riga", 4},
 	                {"Kalnu", "Valmiera", 8},
 	                {"Ultra", "Sigulda", 20},
 	                {"Seb", "Balvi", 40}
 	        };
-
-	        int rowNum = 0;
+			*/
+	        int rowNum = 1;
 	        System.out.println("Creating excel");
 
-	        for (Object[] datatype : datatypes) {
+	        for (int i = 1; i <= tempList.size(); i++) {
+				Marathon maratoni1 = tempList.get(i-1);
+				Row row = sheet.createRow(rowNum);
+	            int colNum = 0;
+	            
+	                Cell cell = row.createCell(colNum);
+	                cell.setCellValue(maratoni1.getName());
+	                Cell cell2 = row.createCell(colNum);
+	                cell2.setCellValue(maratoni1.getPlace());
+	                rowNum++;
+	             
+			}
+	        
+	        
+	        /*for (Marathon [] maratoni1 : maratoni) {
 	            Row row = sheet.createRow(rowNum++);
 	            int colNum = 0;
-	            for (Object field : datatype) {
+	            for (Object field : maratoni1) {
 	                Cell cell = row.createCell(colNum++);
 	                if (field instanceof String) {
 	                    cell.setCellValue((String) field);
@@ -119,6 +163,7 @@ public class OrganizerServiceImpl implements OrganizerService{
 	                }
 	            }
 	        }
+	        */
 
 	        try {
 	            FileOutputStream outputStream = new FileOutputStream(FILE_NAME);
